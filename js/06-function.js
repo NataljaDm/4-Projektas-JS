a563c9f4a2ebb7e4a5818d83ec96ff958a2a1264
Natalja <tasia@inbox.lt>
2023-06-04 21:50:51 +0300

    function done

--- js/06-function.js
+++ js/06-function.js
@@ -1,8 +1,7 @@
 console.clear();
-/*
-FUNCTION - perpanaudojamas logikos blokas
-*/
 
+const x = 3
+console.log(x);
 function empty() {
 }
 
@@ -31,8 +30,6 @@ const myName = sayMyName();
 console.log(myName);
 console.log(sayMyName());
 
-console.clear();
-
 function suma(pirmas, antras) {
     return pirmas + antras;
 }
@@ -55,3 +52,32 @@ const r4 = pirmaRaide('Ona');
 const r5 = pirmaRaide('Martynas');
 
 console.log('Pirmos raides:', r1, r2, r3, r4, r5);
+
+function inicialai(vardas, pavarde) {
+    return `${vardas[0]}.${pavarde[0]}.`;
+}
+
+const ini1 = inicialai('Chuck', 'Norris');
+const ini2 = inicialai('Pamela', 'Anderson');
+
+console.log('Inicialas 1:', ini1);
+console.log('Inicialas 2:', ini2);
+
+
+
+// Sveiki, mano vardas Jonas ir man 99 metai!
+// Sveiki, mano vardas Maryte ir man 88 metai!
+// Sveiki, mano vardas Petras ir man 50 metai!
+// Sveiki, mano vardas Ona ir man 25 metai!
+
+function sakinis(vardas, metai) {
+    return `Sveiki, mano vardas ${vardas} ir man ${metai} metai!`
+}
+const a1 = sakinis (`Jonas`, `99`);
+const a2 = sakinis (`Maryte`, `88`);
+const a3 = sakinis (`Petras`, `50`);
+const a4 = sakinis (`Ona`, `25`);
+console.log(a1);
+console.log(a2);
+console.log(a3);
+console.log(a4);
